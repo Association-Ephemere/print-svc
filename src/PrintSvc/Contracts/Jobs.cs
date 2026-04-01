742f48448700bef0dd81e0847e4c7d8c963981b7
Cleyhan Robasse <74451730+crobasse@users.noreply.github.com>
2026-04-01 16:35:21 +0200

    Update src/PrintSvc/Contracts/Jobs.cs
    Co-authored-by: Robin <114897657+robin-vidal@users.noreply.github.com>

--- src/PrintSvc/Contracts/Jobs.cs
+++ src/PrintSvc/Contracts/Jobs.cs
@@ -2,8 +2,12 @@ namespace PrintSvc.Contracts;
 
 public class Jobs
 {
-    public required string jobId {get; set;}
-    public required string batchId {get; set;}
-    public required string photoStorageKey {get; set;}
-    public int copies {get; set;}
+    [JsonPropertyName("jobId")]
+    public required string JobId {get; set;}
+    [JsonPropertyName("batchId")]
+    public required string BatchId {get; set;}
+    [JsonPropertyName("photoStorageKey")]
+    public required string PhotoStorageKey {get; set;}
+    [JsonPropertyName("copies")]
+    public int Copies {get; set;}
 }
